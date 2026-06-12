d12b1d997a7844b0bdb6c66fdda645f86cc37e14
Nur Mohammod <nurm98371@gmail.com>
2026-06-13 00:43:22 +0600

    Add

--- Dictonaries.py
+++ Dictonaries.py
@@ -15,3 +15,13 @@ planet_to_initial = {planet: planet[0] for planet in planets}
 planet_to_initial
 
 print(planet_to_initial)
+
+print('Saturn ' in planet_to_initial)
+print('Saturn' in planet_to_initial)
+
+for k in numbers:
+    print("{} = {}".format(k,numbers[k]))
+
+print(numbers.items())
+
+print(help(dict))
