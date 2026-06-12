63393d702e61bb04f415e515dd72824f6e29667e
Nur Mohammod <nurm98371@gmail.com>
2026-06-12 10:45:50 +0600

    first commit

---
+++ loops.py
@@ -0,0 +1,43 @@
+plants=['rose','lily','sunflower','daisy']
+for plant in plants:
+    print(plant)
+
+multiplication=(1,2,3,4,5)
+p=1
+for number in multiplication:
+    p=number*p
+
+print(p)
+
+s = 'steganograpHy is the practicE of conceaLing a file, message, image, or video within another fiLe, message, image, Or video.'
+
+
+msg=''
+for letter in s:
+    if letter.isupper():
+        msg=msg+letter
+
+print(msg)
+
+for i in range(1,11):
+    print(i)
+
+print('nursery rhyme')
+
+
+for i in range(11):
+    print(i)
+
+for i in range(5):
+    print("Doing important work. i =", i)
+
+squares = [n**2 for n in range(10)]
+squares
+
+print(squares)
+
+squares = []
+for n in range(10):
+    squares.append(n**2)
+squares
+
