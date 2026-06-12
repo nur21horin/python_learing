d83bda6c9c6d271b4614912531794f0c45c8f14a
Nur Mohammod <nurm98371@gmail.com>
2026-06-12 23:25:51 +0600

    ADd

--- string.py
+++ string.py
@@ -7,8 +7,10 @@ print(x[11:19])
 y='Pluto is a planet'
 
 print(x==y)
-
 name="Nur "
 print(name*5)
-
 print(x==name )
+
+print("my name is nur's")
+print('Pluto\'s a planet!')
+print("look a mountain:/\\")
