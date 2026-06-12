c556ecbdf3faa632e97ee3b8be29dcaba5c2f677
Nur Mohammod <nurm98371@gmail.com>
2026-06-12 23:10:31 +0600

    Fix indentation and formatting issues in loops.py

--- loops.py
+++ loops.py
@@ -1,43 +1,43 @@
-plants=['rose','lily','sunflower','daisy']
-for plant in plants:
-    print(plant)
+        plants=['rose','lily','sunflower','daisy']
+        for plant in plants:
+            print(plant)
 
-multiplication=(1,2,3,4,5)
-p=1
-for number in multiplication:
-    p=number*p
+        multiplication=(1,2,3,4,5)
+        p=1
+        for number in multiplication:
+            p=number*p
 
-print(p)
+        print(p*2)
 
-s = 'steganograpHy is the practicE of conceaLing a file, message, image, or video within another fiLe, message, image, Or video.'
-
-
-msg=''
-for letter in s:
-    if letter.isupper():
-        msg=msg+letter
-
-print(msg)
-
-for i in range(1,11):
-    print(i)
-
-print('nursery rhyme')
+        s = 'Steganography is the practicE of conceaLing a file, message, image, or video within another fiLe, message, image, Or video.'
 
 
-for i in range(11):
-    print(i)
+        msg=''
+        for letter in s:
+            if letter.isupper():
+                msg=msg+letter
 
-for i in range(5):
-    print("Doing important work. i =", i)
+        print(msg)
 
-squares = [n**2 for n in range(10)]
-squares
+        for i in range(1,11):
+            print(i)
 
-print(squares)
+        print('nursery rhyme')
 
-squares = []
-for n in range(10):
-    squares.append(n**2)
-squares
+
+        for i in range(11):
+            print(i)
+
+        for i in range(5):
+            print("Doing important work. i =", i)
+
+        squares = [n**2 for n in range(10)]
+        squares
+
+        print(squares)
+
+        squares = []
+        for n in range(10):
+            squares.append(n**2)
+        squaresy
 
